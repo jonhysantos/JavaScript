9f05e460fa000263a4b0134eea7b479c51e52dd3
Jonhy Santos <Jonhy1905@hotmail.com>
2023-12-10 17:01:16 -0300

    Praticando mais.

--- moduloG/by-myself/Nullish-coalescing-operator/nullish.js
+++ moduloG/by-myself/Nullish-coalescing-operator/nullish.js
@@ -2,6 +2,6 @@ const idade = 0
 console.log(`${idade || 'não informado'}`)
 // tanto com null ou 0 no aperador || <- (ou) ele consta não informado
 
-const age = 0 
+const age = 0
 console.log(`${age ?? 'não informado'}`)
 // Já com o operador nullish -> ?? ele consta não informado caso nulo e 0 caso 0
