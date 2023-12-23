29dd8d9ec147c5c3f1dc9ec3160da9746c672975
Jonhy Santos <Jonhy1905@hotmail.com>
2023-12-23 04:16:16 -0300

    praticando npm e vite

---
+++ curso-one-bit-code-js/aula04/módulos/pratica02/tabuada.js
@@ -0,0 +1,5 @@
+// Usando o import para reduzir o código
+// Exportando a função e as variáveis
+import {verTabuada,numero,res,tab,button,} from "../pratica/tabuada.js"
+verTabuada
+// Importando função e sua variáveis
